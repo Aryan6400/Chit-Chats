75c1721b9d14e45c207798c23b3e70fe883fe716
Aryan Singh <74493183+Aryan6400@users.noreply.github.com>
2024-01-11 03:17:43 +0530

    Added responsiveness to chats page.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Avatar, Grid, GridItem } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import { useSelector, useDispatch } from "react-redux";
 import "./Header.css";
 import { useChat } from "../../context/ChatContext";
 import { changePopupImage, togglePopup } from "../../Features/popupImageSlice";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
     const darkTheme = useSelector((state) => state.darkMode);
-    const { selectedChat } = useChat();
+    const { selectedChat, setSelectedChat } = useChat();
+    const navigate = useNavigate();
     const dispatch=useDispatch();
 
     function getChatName(){
@@ -31,7 +34,17 @@ function Header() {
 
     return (
         <div className={"header" + (darkTheme ? " dark-theme-font" : "")}>
-            <Grid className="name-and-avatar" templateColumns="1fr 10fr 1fr" gap="0.5rem">
+            <Grid className="name-and-avatar" templateColumns="1fr 1fr 10fr 1fr" gap="0.5rem">
+                <GridItem
+                    justifySelf="left"
+                    alignSelf="center"
+                    className="return"
+                >
+                    <KeyboardBackspaceIcon titleAccess="back" className="return-arrow" onClick={() => {
+                        setSelectedChat();
+                        navigate("/welcome");
+                    }}/>
+                </GridItem>
                 <GridItem
                     justifySelf="left"
                     alignSelf="center"
@@ -40,7 +53,6 @@ function Header() {
                     <Avatar src={selectedChat.isGroup ? selectedChat.picturePath : getPicturePath()} onClick={triggerModal} name={selectedChat.isGroup ? selectedChat.name : getChatName()} />
                 </GridItem>
                 <GridItem
-                    fontSize="1.2rem"
                     paddingLeft=".75rem"
                     className="chatName"
                 >
